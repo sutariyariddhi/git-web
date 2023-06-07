0a5414fef4c7c07de3a1c96db2a09b1ad15370dc
riddhi sutariya <sutariyariddhi87@gmail.com>
2023-06-07 20:05:42 +0530

    new function

--- script/cart.js
+++ script/cart.js
@@ -1,3 +1,106 @@
  import nav from "../components/nav.js"
 
 document.getElementById("nav").innerHTML=nav()
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let totalprice = 0;
+for (let i = 0; i < cart.length; i++) {
+  totalprice += cart[i].price * cart[i].qty;
+}
+
+document.getElementById("price").innerHTML = `Total Price: ${totalprice}`;
+
+
+{/* <div class="main-1">
+        <div class="main-1-img">
+          <img
+            class="1-img"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYsQtoYKl7EgvDrtvTsDU5AJieKjtT_b2yiQ&usqp=CAU"
+            alt=""
+          />
+        </div>
+        <div class="main-1-2">
+          <h1 class="main-2-h1">Apple juice</h1>
+        </div>
+
+
+        <div class="main-3">
+          <p class="main-3-p1">+</p>
+          <p class="main-3-p2">2</p>
+          <p class="main-3-p3">-</p>
+        </div>
+
+
+        <div class="main-2">
+     
+            <h1 class="main-2-2h1">$2.99</h1>
+      
+        </div>
+      </div> */}
+
+let cartui=(cart)=>{
+
+  cart.map((ele)=>{
+
+let divimg=document.createElement("div");
+let img=document.createElement("img");
+img.src=ele.image
+
+divimg.append(img)
+divimg.setAttribute("class", "cart-img")
+
+let namediv=document.createElement("div")
+let name=document.createElement("h1")
+name.innerHTML=ele.title
+
+name.setAttribute("class", "title")
+namediv.append(name)
+
+namediv.setAttribute("class","title-main")
+
+
+
+let qtydiv = document.createElement("div")
+let minus=document.createElement("p")
+minus.innerHTML="-"
+let plus=document.createElement("p")
+plus.innerHTML="+"
+let qty=document.createElement("p")
+qty.innerHTML=ele.qty
+
+
+
+minus.setAttribute("class","plus")
+plus.setAttribute("class","mines")
+qty.setAttribute("class","num")
+qtydiv.setAttribute("class","plus-mines")
+qtydiv.append(minus,qty,plus)
+
+
+
+let pricediv=document.createElement("div")
+let price=document.createElement("h1")
+price.innerHTML=ele.price
+price.setAttribute("class", "price")
+pricediv.append(price)
+pricediv.setAttribute("class", "price-heading")
+
+let del = document.createElement("del")
+del.innerHTML = "delete"
+del.addEventListener("click",function(event){
+ event.target.parentNode.remove()
+})
+
+let parent=document.createElement("div")
+parent.append(divimg,namediv,qtydiv,del,pricediv)
+
+parent.setAttribute("class", "main")
+
+document.querySelector("section").append(parent)
+console.log(parent);
+  })
+
+
+}
+
+cartui(cart)
+
--- script/products.js
+++ script/products.js
@@ -8,7 +8,8 @@ let show = (data) => {
 
   data.map((ele) => {
     let img = document.createElement("img");
-    img.src = ele.image;
+    img.src = ele.image
+    img.setAttribute("class","img-pro");
     let title = document.createElement("h3");
     title.innerHTML = ele.title;
     let price = document.createElement("p");
@@ -23,11 +24,14 @@ let show = (data) => {
     btn2.innerHTML = "but now";
     let divbtn = document.createElement("div");
     divbtn.append(btn, btn2);
+    divbtn.setAttribute("class", "divbtn");
     let div = document.createElement("div");
     div.append(img, title, price, category, divbtn);
+    div.setAttribute("class", "main-div");
     document.getElementById("products").append(div);
     btn.addEventListener("click", () => {
       let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      console.log(cart);
       let exists = false;
       cart.map((item, idx) => {
         if (item.id == ele.id) {
@@ -45,5 +49,5 @@ let show = (data) => {
   });
 };
 fetch("https://fakestoreapi.com/products")
-  .then((response) => response.json())
-  .then((data) => show(data));
+.then((response) => response.json())
+.then((data) => show(data));
